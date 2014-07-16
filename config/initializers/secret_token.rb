175f7ec5c8076998a415a250113334c9da110f3c
Jobair Hassan <mohammad_jobair@hotmail.com>
2014-07-16 18:08:38 -0400

    Initial commit for the CMS Project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CMS::Application.config.secret_key_base = '018469af4b75c4b946867d4995ee6ed562304f4efde260174e5514de9e46101215e334a376b037851743238964dd2c51be2af19bec1b913d3f25320729659bfd'
